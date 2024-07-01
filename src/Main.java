d2f7dc2a4fb89b1b868778b7b78d7a12211e4edb
Алёна Бабкина <alena.sencova.13082021@gmail.com>
2024-07-01 22:55:59 +0500

    homework 6 (2)

--- src/Main.java
+++ src/Main.java
@@ -6,49 +6,42 @@ public class Main {
                 System.out.println(i);
             }
         }
-
         {
             System.out.println("Задача 2");
             for (int i = 10; i > 0; i--) {
                 System.out.println(i);
             }
         }
-
         {
             System.out.println("Задача 3");
             for (int i = 0; i <= 17; i = i + 2) {
                 System.out.println(i);
             }
         }
-
         {
             System.out.println("Задача 4");
             for (int i = 10; i >= -10; i--) {
                 System.out.println(i);
             }
         }
-
         {
             System.out.println("Задача 5");
             for (int i = 1904; i < 2096; i = i + 4) {
                 System.out.println(i + " год является высокосным");
             }
         }
-
         {
             System.out.println("Задача 6");
             for (int i = 7; i <= 98; i = i + 7) {
                 System.out.println(i);
             }
         }
-
         {
             System.out.println("Задача 7");
             for (int i = 1; i <= 512; i = i * 2) {
                 System.out.println(i);
             }
         }
-
         {
             System.out.println("Задача 8");
             int salary = 29000;
@@ -59,7 +52,6 @@ public class Main {
             }
             System.out.println(total);
         }
-
         {
             System.out.println("Задача 9");
             int salary = 29000;
@@ -71,7 +63,6 @@ public class Main {
             }
             System.out.println(total);
         }
-
         {
             System.out.println("Задача 10");
             int number = 2;
